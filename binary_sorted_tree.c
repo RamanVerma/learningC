c60242bf59a6abc1d19fa521bfa8d7ae685b32a6
footloose.raman <footloose.raman@gmail.com>
2014-08-05 00:40:25 -0700

    Added more functionality to binary search tree code.

--- binary_sorted_tree.c
+++ binary_sorted_tree.c
@@ -26,9 +26,52 @@ struct person{
    struct person *right;
 };
 /*
+ * buffers for data processing
+ */
+char *fullname1 = NULL;
+char *fullname2 = NULL;
+/*
+ * bstree_insert()  inserts a node into the binary search tree
+ * @root:   the root node of this tree
+ * @node:   the node to be inserted into the tree
+ */
+void bstree_insert(struct person* entry, struct person *root){
+    memset(fullname1, '\0', sizeof(fullname1));
+    memset(fullname2, '\0', sizeof(fullname2));
+    fullname1 = strcat(entry->fname, entry->lname);
+    fullname2 = strcat(root->fname, root->lname);
+    printf("entry full name: %s\n", fullname1);
+    printf("root full name: %s\n", fullname2);
+    /*
+     * make recursive calls to find correct place for this entry
+     */
+    /*
+     * process left child
+     */
+    if(strcmp(fullname1, fullname2) < 0){
+        if(root->left == NULL)
+            root->left = entry;
+        else
+            bstree_insert(entry, root->left);
+    }
+    /*
+     * process left child
+     */
+    if(strcmp(fullname1, fullname2) < 0){
+        if(root->right == NULL)
+            root->right = entry;
+        else
+            bstree_insert(entry, root->right);
+    }
+    return;
+}
+/*
  * main function
  */
 void main(){
+    /*
+     * root for the tree
+     */
     struct person *root = NULL;
     struct person *entry = NULL;
     int count  = 0;
@@ -40,16 +83,34 @@ void main(){
         printf("Failed to allocate memory\n");
         return;
     }
+    /*Allocate memory for helper buffers*/
+    fullname1 = (char *)malloc(sizeof(char) * 2 * MAX_NAME_LEN);
+    if(fullname1 == NULL){
+        printf("Failed to allocate memory\n");
+        return;
+    }
+    /*Allocate memory for helper buffers*/
+    fullname2 = (char *)malloc(sizeof(char) * 2 * MAX_NAME_LEN);
+    if(fullname2 == NULL){
+        printf("Failed to allocate memory\n");
+        return;
+    }
     /*
      * Get the input from the user
      */
     do{
-        /*Allocate memory for variable of type struct person*/
+        /*
+         * Allocate memory for variable of type struct person
+         */
         entry = (struct person *)malloc(sizeof(struct person));
         if(entry == NULL){
             printf("Failed to allocate memory\n");
             return;
         }
+        /*
+         * initialize the input buffer
+         * get the user input
+         */
         memset(buffer, '\0', sizeof(buffer));
         ptr = buffer;
         while((*ptr = getchar()) != '\n'){
@@ -59,7 +120,7 @@ void main(){
          * check if the user entered empty string, else break 
          * the input string into first and last names
          */
-        if(buffer == '\0'){
+        if(strcmp(buffer, "\n") == 0){
             free(entry);
             break;
         }else{
@@ -78,22 +139,28 @@ void main(){
                 count++;
             }
             strncpy(entry->lname, ptr, count);
-            memset(buffer, '\0', sizeof(buffer));
         }
+        /*
+         * if the tree is empty, assign the entry to root
+         * and return
+         */
         if(root == NULL){
             root = entry;
-        }else{
-            /*
-             * insert this entry in the binary search tree
-             */
-//            bstree_insert(entry);
+            return;
         }
+        /*
+         * store the entry into binary search tree
+         */
+        bstree_insert(entry, root);
+        /*
+         * assign the left and right nodes NULL for now
+         */
         entry->left = NULL;
         entry->right = NULL;
+
         ptr = NULL;
         
-        printf("fname: %s\n", entry->fname);
-        printf("lname: %s\n", entry->lname);
-
+//        printf("fname: %s\n", entry->fname);
+//        printf("lname: %s\n", entry->lname);
     }while(1);
 }
